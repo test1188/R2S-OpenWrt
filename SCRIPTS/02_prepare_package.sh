a1fda6bfedac9cf0cf61795774aded495d988844
nicksun98 <44309749+nicksun98@users.noreply.github.com>
2020-08-16 18:19:28 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 clear
+#Kernel
+wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3285.patch | patch -p1
+wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3277.patch | patch -p1
+wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3300.patch | patch -p1
 ##准备工作
 #使用19.07的feed源
 rm -f ./feeds.conf.default
